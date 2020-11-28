64d51b0c6ecef2bd16a30ab7aafd0ed3c4b4632d
jerumanu <emmanueljeru200@gmail.com>
2020-11-28 13:43:29 +0300

    add rootes

--- app/main/views.py
+++ app/main/views.py
@@ -8,4 +8,7 @@ def index():
     '''
     View root page function that returns the index page and its data
     '''
-    return render_template('index.html')
+
+
+    title = 'Welcome to News site '
+    return render_template('index.html',title = title)
