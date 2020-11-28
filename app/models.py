dd700e823233530accd25b1e0701e0992d0539d6
jerumanu <emmanueljeru200@gmail.com>
2020-11-28 13:43:05 +0300

    add class news

---
+++ app/models.py
@@ -0,0 +1,17 @@
+class News:
+
+
+    '''
+    News class to define movie objects
+    '''
+
+    def __init__(self,author,title,description, url,urlToImage,publishedAt,content )
+        self.author = author
+        self.title = title
+        self.description = description
+        self.url = url
+        self.urlToImage = urlToImage
+        self.puplishedAt = puplishedAt
+        Self.content = content
+
+
