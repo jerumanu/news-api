d80d253d19320ea90b6914ad81e1fdcb25bfd9ad
jerumanu <emmanueljeru200@gmail.com>
2020-11-30 13:17:42 +0300

    created class

--- app/models.py
+++ app/models.py
@@ -1,17 +1,33 @@
 class News:
 
-
-    '''
-    News class to define movie objects
-    '''
-
-    def __init__(self,author,title,description, url,urlToImage,publishedAt,content )
-        self.author = author
+    def __init__(self,id,title,overview,poster,vote_average):
+        self.id =id
         self.title = title
-        self.description = description
-        self.url = url
-        self.urlToImage = urlToImage
-        self.puplishedAt = puplishedAt
-        Self.content = content
+        self.overview = overview
+        self.poster = "https://image.tmdb.org/t/p/w500/" + poster
+        self.vote_average = vote_average
 
+
+
+
+class Review:
+    all_reviews = []
+    def __init__(self,news_id,title,imageurl,review):
+        self.news_id = news_id
+        self.title = title
+        self.imageurl = imageurl
+        self.review = review
+    def save_review(self):
+        Review.all_reviews.append(self)
+    @classmethod
+    def clear_reviews(cls):
+        Review.all_reviews.clear()
+    @classmethod
+    def get_reviews(cls,id):
+        response = []
+        for review in cls.all_reviews:
+            if review.news_id == id:
+                response.append(review)
+        return response
+        
 
