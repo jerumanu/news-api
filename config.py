d5eda5ee9c22a881edbbbb5089cb52d9c0e2b3f3
jerumanu <emmanueljeru200@gmail.com>
2020-11-30 13:16:37 +0300

    add html

--- config.py
+++ config.py
@@ -1,9 +1,10 @@
- import os
+import os
 
 class Config:
 
-    NEWS_API_BASE_URL ='https://api.themoviedb.org/3/movie/{}?api_key={}'
-    NEWS_API_KEY = os.environ.get('MOVIE_API_KEY')
+    NEWS_API_BASE_URL ='http://newsapi.org/v2/everything?q={}&from=2020-10-30&sortBy=publishedAt&apiKey={}'
+    NEWS2_API_BASE_URL = 'http://newsapi.org/v2/top-headlines?sources={}&apiKey={}'
+    NEWS_API_KEY = os.environ.get('NEWS_API_KEY')
     SECRET_KEY = os.environ.get('SECRET_KEY')
 
 
